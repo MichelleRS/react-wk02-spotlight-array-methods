e12eb38cb26cb9afbbe972ce5683cc4b5d632e97
Michelle Ross <miross629@gmail.com>
2023-01-18 12:24:57 -0800

    Passed: get an array of all shoes using .reduce

--- reduce.js
+++ reduce.js
@@ -1,8 +1,16 @@
 // INPUT: the array of brands from data.js
-// OUTPUT: an array of all the sneakers brands
+// OUTPUT: an array of all the sneakers shoes
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  // using a for loop:
+  //   let sneaks = [];
+  //   for (let brand of brands) {
+  //     sneaks = [...sneaks, ...brand.shoes];
+  //   }
+  //   return sneaks;
+  return brands.reduce((acc, brand) => [...acc, ...brand.shoes], []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
